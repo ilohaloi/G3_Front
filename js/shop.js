109eb3e7db6005dd0e30e9e795e5abf58e9d26f4
kiki77596 <kiki77596@gmail.com>
2024-11-08 18:58:29 +0800

    fix cart img

--- js/shop.js
+++ js/shop.js
@@ -32,7 +32,7 @@ export function addTocart(item,qty,action) {
         name: item.name,
         price: item.price,
         qty:qty,
-        imgs: item.img1
+        imgs: item.imgs[0]
     }
 
     if (!localStorage.getItem('cart')) { 
